25ad8e4ace965db0686a0754c7a16203fa740783
arthur <zhiyuan@gmail.com>
2024-03-27 22:15:01 -0700

    Mar 27 night commit

--- src/main/java/com/arthur/learn/mybatis/mapper/ParameterMapper.java
+++ src/main/java/com/arthur/learn/mybatis/mapper/ParameterMapper.java
@@ -1,12 +1,23 @@
 package com.arthur.learn.mybatis.mapper;
 
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
 
 import com.arthur.learn.mybatis.entity.User;
 
 public interface ParameterMapper {
 
     List<User> getAllUsers();
+
     User getUserById(int id);
+
     User checkLogin(String name, String password);
+
+    User checkLoginByMap(Map<String, Object> map);
+
+    int insertUser(User user);
+
+    User checkLoginByParam(@Param("name")String name, @Param("password")String password);
 }
--- src/test/java/MyBatisTest.java
+++ src/test/java/MyBatisTest.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -59,6 +61,33 @@ public class MyBatisTest {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         ParameterMapper parameterMapper = sqlSession.getMapper(ParameterMapper.class);
         User user = parameterMapper.checkLogin("Zhang Ziyi", "fast1234");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testCheckLoginByMap() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper parameterMapper = sqlSession.getMapper(ParameterMapper.class);
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Zhang Ziyi");
+        map.put("password", "fast1234");
+        User user = parameterMapper.checkLoginByMap(map);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper parameterMapper = sqlSession.getMapper(ParameterMapper.class);
+        int ret = parameterMapper.insertUser(new User(null, "Marcia Dong", "123456", "San Diego", "1388888888"));
+        System.out.println(ret);
+    }
+
+    @Test
+    public void testCheckLoginByParam() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper parameterMapper = sqlSession.getMapper(ParameterMapper.class);
+        User user = parameterMapper.checkLoginByParam("Zhang Ziyi", "fast1234");
         System.out.println(user);
     }
 }
